03f3090e348bcc086ad6fa40df886140c0987c5f
matyiandras <a2001matyas@gmail.com>
2023-12-02 19:28:57 +0100

    Fix header layout in T-shirt generator

--- tshirt-generator/src/components/layout/Header.js
+++ tshirt-generator/src/components/layout/Header.js
@@ -22,22 +22,11 @@ export const Header = () => {
         <Disclosure as="nav" className="bg-cyan-800">
             {({ open }) => (
                 <>
-                    <div className="mx-auto container sm:px-6 lg:px-4">
+                    <div className="mx-auto container sm:px-6 px-4">
                         <div className="relative flex h-24 items-center justify-between">
-                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                                {/* Mobile menu button*/}
-                                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 m-1 text-white hover:bg-cyan-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                                    <span className="absolute -inset-0.5" />
-                                    <span className="sr-only">Open main menu</span>
-                                    {open ? (
-                                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                                    ) : (
-                                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                                    )}
-                                </Disclosure.Button>
-                            </div>
-                            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                                <div className="flex items-center ml-10 md:ml-0">
+                            
+                            <div className="flex flex-1 items-center justify-start sm:items-stretch sm:justify-start">
+                                <div className="flex items-center">
                                     <div className='rounded-xl border border-4 py-2 px-4'>
                                         <p className='font-bold text-2xl text-white '>T-shirt generator POC</p>
                                     </div>
@@ -59,6 +48,18 @@ export const Header = () => {
                                         ))}
                                     </div>
                                 </div>
+                            </div>
+                            <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+                                {/* Mobile menu button*/}
+                                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 m-1 text-white hover:bg-cyan-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="absolute -inset-0.5" />
+                                    <span className="sr-only">Open main menu</span>
+                                    {open ? (
+                                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                                    ) : (
+                                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                                    )}
+                                </Disclosure.Button>
                             </div>
 
                         </div>
